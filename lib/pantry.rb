59057364f2b178cc84dac17bcc473719951205f3
Emmie Hayes <emmiehayes2@gmail.com>
2018-06-05 09:17:34 -0600

    iteration 1 complete

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Pantry
+  attr_reader :stock
 
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(item)
+    @stock[item]
+  end
+
+  def restock(item, qty)
+    if @stock.keys.include?(item)
+      @stock[item] += qty
+    else
+      @stock[item] = qty
+    end
+    @stock
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,31 @@
-require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/pantry'
+require './lib/recipe'
 
 class PantryTest < Minitest::Test
+
+  def test_it_has_attributes
+    p = Pantry.new
+    assert_equal ({}), p.stock
+  end
+
+  def test_it_can_check_stock
+    p = Pantry.new
+    assert p.stock.empty?
+  end
+
+  def test_it_can_add_items_to_stock
+    p = Pantry.new
+    p.restock('Cheese', 10)
+    assert_equal ({'Cheese' => 10}), p.stock
+  end
+
+  def test_it_can_add_to_current_stock_level
+    p = Pantry.new
+    p.restock('Cheese', 10)
+    p.restock('Cheese', 20)
+    assert_equal ({'Cheese' => 30}), p.stock
+  end
 
 end
